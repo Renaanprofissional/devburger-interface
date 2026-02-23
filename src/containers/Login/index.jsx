812264b92fa9b0dbc18bce3ee2ce7db1533b4397
Renaanprofissional <renaan.profissional@gmail.com>
2026-02-23 19:25:29 -0300

    feat: login structure

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,7 +1,41 @@
+import {
+  Button,
+  Container,
+  Form,
+  InputContainer,
+  LeftContainer,
+  Link,
+  RightContainer,
+  Title,
+} from './styles';
+import Logo from '../../assets/logo.png';
+
 export function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="Logo do DevBurger" />
+      </LeftContainer>
+      <RightContainer>
+        <Title>
+          Olá, seja bem vindo ao <span>DevBurger</span>, Acesse com seu
+          <span> Login e senha.</span>
+        </Title>
+        <Form>
+          <InputContainer>
+            <label>Email</label>
+            <input type="email" placeholder="Digite seu email" />
+          </InputContainer>
+
+          <InputContainer>
+            <label>Senha</label>
+            <input type="password" placeholder="Digite sua senha" />
+          </InputContainer>
+          <Link>Esqueci minha senha.</Link>
+          <Button>Entrar</Button>
+        </Form>
+        <Link>Não tem conta? Cadastre-se</Link>
+      </RightContainer>
+    </Container>
   );
 }
