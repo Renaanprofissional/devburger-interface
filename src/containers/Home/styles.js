c20153b476b5f587c0df334d96f683e8d6462a28
Renaanprofissional <renaan.profissional@gmail.com>
2026-02-28 10:11:18 -0300

    feat: adding category carousel

---
+++ src/containers/Home/styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+import Background from "../../assets/bg2.png"
+import BannerHome from "../../assets/banner-home.svg";
+
+export const Banner = styled.div`
+  background: url('${BannerHome}');
+  background-size: cover;
+  background-position: center;
+  height: 400px;
+
+  h1{
+    font-family: 'Roade Rage', sans-serif;
+    font-size: 80px;
+    color: #f4f4f4;
+    position: absolute;
+    right: 20%;
+    top: 20%;
+  }
+  
+  `
+export const Container = styled.section`
+     background: linear-gradient(
+    rgba(255, 255, 255, 0.8),
+    rgba(255, 255, 255, 0.8)
+     ),
+     url('${Background}');
+     background-size: cover;
+     height: 500px;
+`
+
+export const Content = styled.div``
--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,5 +1,5 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { set, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import * as yup from 'yup';
@@ -19,15 +19,20 @@ import Logo from '../../assets/logo.png';
 import { api } from '../../services/api';
 
 export function Login() {
-
   const navigate = useNavigate();
 
   const schema = yup
-  .object({
-    email: yup.string().email('Digite um email válido').required('O email é obrigatório'),
-    password: yup.string().min(6, 'A senha deve ter no mínimo 6 caracteres').required('A senha é obrigatória'),
-  })
-  .required();
+    .object({
+      email: yup
+        .string()
+        .email('Digite um email válido')
+        .required('O email é obrigatório'),
+      password: yup
+        .string()
+        .min(6, 'A senha deve ter no mínimo 6 caracteres')
+        .required('A senha é obrigatória'),
+    })
+    .required();
 
   const {
     register,
@@ -37,56 +42,68 @@ export function Login() {
     resolver: yupResolver(schema),
   });
   const onSubmit = async (data) => {
-    const response = await toast.promise(
+    const {
+      data: { token },
+    } = await toast.promise(
       api.post('/sessions', {
-      email: data.email,
-      password: data.password
-    }),
-    {
-      pending: 'Verificando suas credenciais...',
-      success: {
-        render() {
-          setTimeout(() => {
-            navigate('/');
-          }, 2000);
-          return 'Login realizado com sucesso!';
-        }
+        email: data.email,
+        password: data.password,
+      }),
+      {
+        pending: 'Verificando suas credenciais...',
+        success: {
+          render() {
+            setTimeout(() => {
+              navigate('/');
+            }, 2000);
+            return 'Login realizado com sucesso!';
+          },
+        },
+        error:
+          'Erro ao fazer login. Verifique suas credenciais e tente novamente.',
       },
-      error: 'Erro ao fazer login. Verifique suas credenciais e tente novamente.',
-    }
-    )
-    
-    console.log(response);
-  };
+    );
 
+    localStorage.setItem('token', token);
+  };
 
   return (
     <Container>
       <LeftContainer>
-        <img src={Logo} alt="Logo do DevBurger"/>
+        <img src={Logo} alt="Logo do DevBurger" />
       </LeftContainer>
       <RightContainer>
         <Title>
           <span>Olá,</span> seja bem vindo ao <span>DevBurger</span>,
           <br />
-           <span>Acesse</span> com seu
+          <span>Acesse</span> com seu
           <span> Login e senha.</span>
         </Title>
         <Form onSubmit={handleSubmit(onSubmit)}>
           <InputContainer>
             <label>Email</label>
-            <input type="email" placeholder="Digite seu email" {...register("email")} />
+            <input
+              type="email"
+              placeholder="Digite seu email"
+              {...register('email')}
+            />
             {errors.email && <p>{errors.email.message}</p>}
           </InputContainer>
 
           <InputContainer>
             <label>Senha</label>
-            <input type="password" placeholder="Digite sua senha" {...register("password")} />
+            <input
+              type="password"
+              placeholder="Digite sua senha"
+              {...register('password')}
+            />
             {errors.password && <p>{errors.password.message}</p>}
           </InputContainer>
           <Button type="submit">Entrar</Button>
         </Form>
-        <p>Não tem conta? <Link to="/cadastro">Cadastre-se</Link></p>
+        <p>
+          Não tem conta? <Link to="/cadastro">Cadastre-se</Link>
+        </p>
       </RightContainer>
     </Container>
   );
