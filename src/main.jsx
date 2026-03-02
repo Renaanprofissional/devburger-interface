b53107f930515b2a26c572f54cbd383ff9d77ef5
Renaanprofissional <renaan.profissional@gmail.com>
2026-03-02 19:26:59 -0300

    feat: creating initial context

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import { ToastContainer } from 'react-toastify';
 
 import { router } from './routes';
 import GlobalStyles from './styles/globalStyles';
-
-
+import AppProvider from './hooks';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AppProvider>
       <RouterProvider router={router} />
       <GlobalStyles />
-      <ToastContainer autoClose={2000} theme='colored'/>
-  </React.StrictMode>
-)
+      <ToastContainer autoClose={2000} theme="colored" />
+    </AppProvider>
+  </React.StrictMode>,
+);
