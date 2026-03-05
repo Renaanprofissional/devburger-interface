1a35b409e07efca39d9f2de73acf2f17445ff6cb
Renaanprofissional <renaan.profissional@gmail.com>
2026-03-05 20:48:28 -0300

    feat: add api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3001" || "https://dev-burguer-api.vercel.app"
+    baseURL: "https://dev-burguer-api.vercel.app"
 })
 
 api.interceptors.request.use( (config) => {
