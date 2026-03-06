2939927ebf4804f1a926b19546e996ff58c24cce
Renaanprofissional <renaan.profissional@gmail.com>
2026-03-05 21:06:59 -0300

    feat: add prodution

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3001" || "https://dev-burguer-api.vercel.app"
+    baseURL: window.location.hostname === "localhost"
+        ? "http://localhost:3001"
+        : "https://dev-burguer-api.vercel.app"
 })
 
-api.interceptors.request.use( (config) => {
+api.interceptors.request.use((config) => {
     const userData = localStorage.getItem('devburger:userData');
 
     const token = userData && JSON.parse(userData).token;
@@ -12,4 +14,4 @@ api.interceptors.request.use( (config) => {
     config.headers.Authorization = `Bearer ${token}`;
 
     return config;
-})
+});
